85fe89b051e77501b902b4b20b192050d07f7688
Uladzimir_Ivashkevich <d45morozko@tut.by>
2017-07-12 13:54:17 +0300

    v0.3

--- main.go
+++ main.go
@@ -1,11 +1,32 @@
 package main
 
-import "github.com/go-martini/martini"
+import (
+	"database/sql"
+	"log"
+	"os"
+
+	"github.com/go-martini/martini"
+	"github.com/lib/pq"
+)
 
 func main() {
 	m := martini.Classic()
 	m.Get("/", func() string {
 		return "Hello World"
 	})
+	m.Get("/db", openDb)
 	m.Run()
+}
+
+func openDb() *sql.DB {
+	url := os.Getenv("DATABASE_URL")
+	connection, _ := pq.ParseURL(url)
+	connection += " sslmode=require"
+
+	db, err := sql.Open("postgres", connection)
+	if err != nil {
+		log.Println(err)
+	}
+
+	return db
 }
