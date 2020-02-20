2e0c6a1757d96fb6b854889bbfb6412334096df7
IvashkevichUU <d45morozko@tut.by>
2020-02-20 18:21:57 +0300

    refactoring js

--- templates/js/custom.js
+++ templates/js/custom.js
@@ -1,3 +1,5 @@
+var changeName;
+
 //Close alert
 $(function(){
     window.setTimeout(function(){
@@ -29,34 +31,96 @@ var int = setInterval(function() {
 
 //Spinner anim
 var spinnerAnimation = function() {
-var widthProgress = $('.progress').width()-6;
+var widthProgress = $('.progress').width();
 var progressStop = $('.progress .bg-success');
-var widthProgressStop = (progressStop.position().left+progressStop.width()/2)-6;
+var widthProgressStop = (progressStop.position().left+(progressStop.width()/2))-8;
+var arrowStop = widthProgressStop/(widthProgress/100);
 $('.spinner-arrow').slideDown();
-//console.log(widthProgress);
-//console.log(widthProgressStop);
+// console.log(widthProgress);
+// console.log(progressStop.position().left);
+// console.log(progressStop.width());
+// console.log(widthProgressStop);
+// console.log(arrowStop);
+spinnerChangeName();
     $('.spinner-arrow').animate({
-        "margin-left":"+="+widthProgress+"px"
+        "left":"+=100%"
     }, 500, function () {
         $(this).animate({
-            "margin-left": "0px"
+            "left": "-8px"
         }, 1000, function () {
             $(this).animate({
-                "margin-left":"+="+widthProgress+"px"
+                "left":"+=100%"
             }, 1500, function () {
                 $(this).animate({ 
-                    "margin-left": "0px"
+                    "left": "0%"
                 }, 2000, function () {
                     $(this).animate({
-                        "margin-left":"+="+widthProgressStop+"px"
+                        "left":"+="+arrowStop+"%"
                     }, 2500, function () {
-                        $(this).css("background", "red")
+                        $(this).css("background", "red");
+                        clearInterval(changeName);
+                        $('.round-tittle').text('Winner: '+$('.round-tittle').text());
                     });
                 });
             });
         });
     });
+    
 };
 
+//Change player name
+var spinnerChangeName = function() {
+var progressArrow = $('.progress-container .spinner-arrow');
+var progressPlayers = progressArrow.next().children('.progress-bar');
+      var posEl;
+      var widthEl;
+      var posArrow;
+//console.log(progressPlayers);
+//console.log(progressPlayers.length);
+//console.log(progressArrow.position().left);
 
+  changeName = setInterval(function() {
 
+// var i = 0;
+// for (i = 0; i < progressPlayers.length; i++) {
+//  //console.log(progressPlayers);
+//  //console.log(progressPlayers[i]);
+
+// var element = progressPlayers[i];
+
+//       minPosEl = $(element).position().left;
+//       widthEl = $(element).width();
+//       posArrow = progressArrow.position().left;
+//       maxPosEl = minPosEl+widthEl;
+
+//       if (minPosEl < posArrow){
+
+//         if (posArrow < maxPosEl) {
+//         console.log($(element).text());
+//         $('.round-tittle').text($(element).text());
+//       }
+//     }
+// }
+
+    progressPlayers.map(function(indx, element){
+      
+      minPosEl = $(element).position().left;
+      widthEl = $(element).width();
+      posArrow = progressArrow.position().left;
+      maxPosEl = minPosEl+widthEl;
+
+      // console.log('posEl',minPosEl);
+      // console.log('posArrow',posArrow);
+      // console.log('widthEl',maxPosEl);
+
+      if (minPosEl <= posArrow){
+       if (posArrow <= maxPosEl) {
+        // console.log(indx);
+        //console.log($(element).text());
+        $('.round-tittle').text($(element).text());
+      }
+    }
+    });
+  }, 100);
+
+};
